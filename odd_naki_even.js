d68a7e4c4038d37d8d946061d0785dfe5d80f441
nadim50 <mdnadimmir50@gmail.com>
2022-08-06 04:35:17 +0600

    first commit

---
+++ odd_naki_even.js
@@ -0,0 +1,19 @@
+///declare function
+
+function oddNakiEven(value) {
+    if (value % 2 == 0) {
+        return console.log('Even number');
+    }
+
+    else {
+        console.log('Odd number');
+    }
+
+}
+
+///called function
+
+let num = 46;
+oddNakiEven(num);
+
+
